5b970a0bead07d70e5832227cfa56f24f410fc3f
DoHoKun <2727461997@qq.com>
2024-09-30 08:19:29 +0800

    update

--- src/jvzhen.java
+++ src/jvzhen.java
@@ -1,5 +1,23 @@
 public class jvzhen {
     public static void main(String[] args) {
-
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+    };
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] newMatrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                newMatrix[i][j] = matrix[rows-1-i][cols-1-j];
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(newMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
